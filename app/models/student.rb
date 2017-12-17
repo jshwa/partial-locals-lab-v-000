8a2507a1d13623d29ea93b78b0b8eec8294db839
Joshua Palacios <palacios.joshua@gmail.com>
2017-12-17 03:56:00 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_name)
+    Student.find_by(name: student_name)
+  end
 end
